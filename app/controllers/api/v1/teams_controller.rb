45b70e477bb6b595d5980bb2e1a76d670d6df728
Pierre Boulanger <pierre.boulanger1@gmail.com>
2018-06-07 14:38:57 +0200

    add second endpoint show + search tool

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,5 +1,19 @@
 class Api::V1::TeamsController < Api::V1::BaseController
+  before_action :set_team, only: [ :show ]
+
+
   def index
-    @teams = policy_scope(Team)
+    params[:search].blank? ? @teams = policy_scope(Team) : @teams = policy_scope(Team).where('name ILIKE ?', "%#{params[:search]}%")
   end
+
+  def show
+  end
+
+  private
+
+  def set_team
+    @team = Team.find(params[:id])
+    authorize @team
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :teams, only: [ :index ]
+      resources :teams, only: [ :index, :show ]
     end
   end
 
